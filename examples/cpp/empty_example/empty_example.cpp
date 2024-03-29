62354bca3f2d028a22a4e8d267129c1aa3026784
Aljaž <aljaz.francic@gmail.com>
2024-03-29 11:25:00 +0100

    Modify empty example

--- examples/cpp/empty_example/empty_example.cpp
+++ examples/cpp/empty_example/empty_example.cpp
@@ -2,8 +2,8 @@
  * Empty example
  */
 
-#include <iostream>
 #include <opendaq/opendaq.h>
+#include <iostream>
 
 using namespace daq;
 
@@ -11,6 +11,26 @@ int main(int /*argc*/, const char* /*argv*/[])
 {
     // Create an Instance, loading modules at MODULE_PATH
     const InstancePtr instance = Instance(MODULE_PATH);
+
+    std::cout << "DEVICES:" << std::endl;
+    daq::ListPtr<daq::IDeviceInfo> infos = instance.getAvailableDevices();
+    for (const auto& info : infos)
+        std::cout << "  Name: " << info.getName() << ", Address: " << info.getConnectionString()
+                  << ", Serial number: " << info.getSerialNumber() << std::endl;
+
+
+    auto strings = std::vector{"daq.opcua://192.168.56.101/", "daqref://device0"};
+
+    for (auto& str : strings)
+    {
+        auto dev = instance.addDevice(str);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        std::cout << "last value: " << dev.getChannels()[0].getSignals()[0].getLastValue() << std::endl;
+
+        std::cout << "localID: " << dev.getLocalId() << std::endl;
+    }
 
     std::cout << "Press \"enter\" to exit the application..." << std::endl;
     std::cin.get();
