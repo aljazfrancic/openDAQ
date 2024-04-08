49b00d19dff69eaaf63384d4f88416c687db68b0
Aljaž <aljaz.francic@gmail.com>
2024-04-08 17:07:29 +0200

    Wonky test

--- examples/cpp/empty_example/empty_example.cpp
+++ examples/cpp/empty_example/empty_example.cpp
@@ -1,18 +1,43 @@
-/**
- * Empty example
- */
-
-#include <iostream>
+#include <opendaq/channel_impl.h>
 #include <opendaq/opendaq.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 using namespace daq;
+using namespace std::chrono_literals;
 
 int main(int /*argc*/, const char* /*argv*/[])
 {
-    // Create an Instance, loading modules at MODULE_PATH
-    const InstancePtr instance = Instance(MODULE_PATH);
+    auto instance = Instance();
 
-    std::cout << "Press \"enter\" to exit the application..." << std::endl;
-    std::cin.get();
-    return 0;
+    auto root = instance.getRootDevice();
+
+    FolderConfigPtr signals = root.getItem("Sig");
+    auto signal = Signal(instance.getContext(), signals, "sig1");
+    signals.addItem(signal);
+
+    auto chans = root.getChannels();
+
+    instance.addStandardServers();
+
+    // test_last_value_data_packet
+    // TODO
+
+    // test_last_value_signal
+    auto descriptor = DataDescriptorBuilder().setSampleType(SampleType::Float64).build();  // CHANGE
+
+    auto packet = DataPacket(descriptor, 5);
+
+    auto data = static_cast<double*>(packet.getData());  // CHANGE
+    data[4] = 0.3;                                       // CHANGE
+
+    // test_last_value_range
+    // TODO
+
+    signal.setDescriptor(descriptor);
+    signal.sendPacket(packet);
+
+    while (true)
+        std::this_thread::sleep_for(100ms);
 }
