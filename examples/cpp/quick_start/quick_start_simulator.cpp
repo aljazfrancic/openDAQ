275d2b493199eba90d6ca6d6eac37f22ecf42101
Aljaž <aljaz.francic@gmail.com>
2025-04-28 16:30:07 +0200

    Modify example

--- examples/cpp/quick_start/quick_start_simulator.cpp
+++ examples/cpp/quick_start/quick_start_simulator.cpp
@@ -1,45 +1,45 @@
-/**
- * Part of the openDAQ stand-alone application quick start guide. Starts
- * an openDAQ server on localhost that contains a simulated device.
- */
-
 #include <opendaq/opendaq.h>
 #include <iostream>
+
+#include "coreobjects/argument_info_factory.h"
+#include "coreobjects/callable_info_factory.h"
 
 using namespace daq;
 
 int main(int /*argc*/, const char* /*argv*/[])
 {
-    using namespace std::chrono_literals;
-    StringPtr loggerPath = "ref_device_simulator.log";
+    const InstancePtr instance = InstanceBuilder().addDiscoveryServer("mdns").setRootDevice("daqref://device0").build();
 
-    auto users = List<IUser>();
-    users.pushBack(User("opendaq", "$2b$10$bqZWNEd.g1R1Q1inChdAiuDr5lbal33bBNOehlCwuWcxRH5weF3hu")); // password: opendaq
-    users.pushBack(User("root", "$2b$10$k/Tj3yqFV7uQz42UCJK2n.4ECd.ySQ2Sfd81Kx.xfuMOeluvA/Vpy", {"admin"})); // password: root
-    const AuthenticationProviderPtr authenticationProvider = StaticAuthenticationProvider(true, users);
-
-    PropertyObjectPtr config = PropertyObject();
-    config.addProperty(StringProperty("Name", "Reference device simulator"));
-    config.addProperty(StringProperty("LocalId", "RefDevSimulator"));
-    config.addProperty(StringProperty("SerialNumber", "sim01"));
-    config.addProperty(BoolProperty("EnableLogging", true));
-    config.addProperty(StringProperty("LoggingPath", loggerPath));
-
-    const InstancePtr instance = InstanceBuilder().addModulePath(MODULE_PATH)
-                                                  .addDiscoveryServer("mdns")
-                                                  .setRootDevice("daqref://device1", config)
-                                                  .setLogger(Logger(loggerPath))
-                                                  .setAuthenticationProvider(authenticationProvider)
-                                                  .build();
-
-    auto refDevice = instance.addDevice("daqref://device0");
-    refDevice.setPropertyValue("EnableProtectedChannel", true);
     const auto servers = instance.addStandardServers();
 
     for (const auto& server : servers)
         server.enableDiscovery();
-        
-    std::cout << "Press \"enter\" to exit the application..." << std::endl;
+
+    const auto sumXYProp =
+        FunctionPropertyBuilder(
+            "SumXY",
+            FunctionInfo(ctInt,
+                         List<IArgumentInfo>(ArgumentInfo("Val1", ctInt), ArgumentInfo("Val2", ctInt), ArgumentInfo("Val3", ctString))))
+            .build();
+
+    instance.addProperty(sumXYProp);
+
+    auto myFun = Function(
+        [](Int val1, Int val2, StringPtr val3)
+        {
+            std::cout << "SumXY str is: " << val3 << "\n";
+            return Integer(val1 + val2);
+        });
+
+    instance.setPropertyValue("SumXY", myFun);
+
+    FunctionPtr sum = instance.getPropertyValue("SumXY");
+
+    std::cout << "Result sum: " << sum(2, 3, "example") << "\n";
+
+    std::cout << "Result myFun: " << myFun(2, 3, "example") << "\n";
+
+    std::cout << "Press \"enter\" to exit the application..." << "\n";
     std::cin.get();
     return 0;
 }
